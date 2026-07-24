42ba5083f1ff7086690c5b44066fdd3dd87fc727
huiwentong <1120267329@qq.com>
2026-07-25 00:32:07 +0800

    一个愚蠢的小bug

--- manager/app/validation.py
+++ manager/app/validation.py
@@ -56,7 +56,7 @@ def validate_tool(
         result.add_error("【工具描述】不能为空")
     if not url.strip():
         result.add_error("【工具链接】不能为空")
-    elif not url.startswith(" http://","https://"):
+    elif not url.startswith("https://"):
         result.add_warning("【工具链接】建议以 https:// 开始")
     if not category.strip():
         result.add_error("【工具分类】不能为空，请选择一个分类")
